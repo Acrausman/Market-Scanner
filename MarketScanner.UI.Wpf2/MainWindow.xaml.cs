5ad9f93dde2e770539096d9da0b409abcc824094
Adam Crausman <55885755+Acrausman@users.noreply.github.com>
2025-09-29 16:21:38 -0400

    New axes and volume bars

--- MarketScanner.UI.Wpf2/MainWindow.xaml.cs
+++ MarketScanner.UI.Wpf2/MainWindow.xaml.cs
@@ -1,138 +1,222 @@
 ﻿using OxyPlot;
-using OxyPlot.Annotations;
 using OxyPlot.Axes;
 using OxyPlot.Series;
+using OxyPlot.Wpf;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
 
-namespace MarketScanner.UI
+namespace MarketScanner.UI.Wpf
 {
     public partial class MainWindow : Window
     {
-        public PlotModel MyPlotModel { get; private set; }
-        private Dictionary<string, LineSeries> priceSeriesMap;
-        private Dictionary<string, LineSeries> rsiSeriesMap;
-        private Dictionary<string, LineSeries> smaSeriesMap;
-        private Dictionary<string, LineSeries> upperBandSeriesMap;
-        private Dictionary<string, LineSeries> lowerBandSeriesMap;
-        private Dictionary<string, LineSeries> volumeSeriesMap;
-        private MarketDataEngine engine;
+        private PlotModel MyPlotModel;
 
-        private const double RSIOverbought = 70;
-        private const double RSIOversold = 30;
+        private LineSeries priceSeries;
+        private LineSeries smaSeries;
+        private AreaSeries bollingerArea;
+        private RectangleBarSeries volumeSeries;
+        private LineSeries rsiSeries;
+
+        private List<DataPoint> pricePoints = new();
+        private List<double> volumePoints = new();
+        private Random rng = new();
+
+        private DispatcherTimer timer;
+
+        private int smaPeriod = 14;
+        private double bollingerMultiplier = 2.0;
+        private int maxPoints = 50;
 
         public MainWindow()
         {
             InitializeComponent();
+            SetupChart();
+            GenerateInitialData();
+            StartLiveDummyData();
+            DataContext = this;
+        }
 
-            MyPlotModel = new PlotModel { Title = "Market Scanner - Price + RSI + SMA + Volume" };
+        private void SetupChart()
+        {
+            MyPlotModel = new PlotModel { Title = "Market Scanner Demo" };
 
-            // Axes
-            var priceAxis = new LinearAxis { Position = AxisPosition.Left, Title = "Price" };
-            var rsiAxis = new LinearAxis { Position = AxisPosition.Right, Title = "RSI", Minimum = 0, Maximum = 100 };
-            var volAxis = new LinearAxis { Position = AxisPosition.Bottom, Title = "Volume", Minimum = 0, Key = "VolumeAxis" };
+            // DateTime X-axis
+            var dateAxis = new DateTimeAxis
+            {
+                Position = AxisPosition.Bottom,
+                StringFormat = "HH:mm:ss",
+                Title = "Time",
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot,
+                IntervalType = DateTimeIntervalType.Seconds,
+                MinorIntervalType = DateTimeIntervalType.Seconds
+            };
+            MyPlotModel.Axes.Add(dateAxis);
+
+            // Price axis (left)
+            var priceAxis = new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                Key = "PriceAxis",
+                Title = "Price",
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot
+            };
             MyPlotModel.Axes.Add(priceAxis);
+
+            // RSI axis (right)
+            var rsiAxis = new LinearAxis
+            {
+                Position = AxisPosition.Right,
+                Key = "RSIAxis",
+                Title = "RSI",
+                Minimum = 0,
+                Maximum = 100,
+                StartPosition = 0.25,
+                EndPosition = 0.5,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dot
+            };
             MyPlotModel.Axes.Add(rsiAxis);
 
-            myPlot.Model = MyPlotModel;
-
-            priceSeriesMap = new Dictionary<string, LineSeries>();
-            rsiSeriesMap = new Dictionary<string, LineSeries>();
-            smaSeriesMap = new Dictionary<string, LineSeries>();
-            upperBandSeriesMap = new Dictionary<string, LineSeries>();
-            lowerBandSeriesMap = new Dictionary<string, LineSeries>();
-            volumeSeriesMap = new Dictionary<string, LineSeries>();
-
-            var symbols = new List<string> { "AAPL", "MSFT", "NVDA" };
-            engine = new MarketDataEngine(symbols);
-
-            foreach (var s in symbols)
+            // Volume axis (bottom)
+            var volumeAxis = new LinearAxis
             {
-                // Price
-                var pSeries = new LineSeries { Title = s + " Price", YAxisKey = priceAxis.Key };
-                priceSeriesMap[s] = pSeries;
-                MyPlotModel.Series.Add(pSeries);
+                Position = AxisPosition.Right,
+                Key = "VolumeAxis",
+                Minimum = 0,
+                StartPosition = 0,
+                EndPosition = 0.2,
+                Title = "Volume"
+            };
+            MyPlotModel.Axes.Add(volumeAxis);
 
-                // RSI
-                var rSeries = new LineSeries { Title = s + " RSI", YAxisKey = rsiAxis.Key };
-                rsiSeriesMap[s] = rSeries;
-                MyPlotModel.Series.Add(rSeries);
+            // Price series
+            priceSeries = new LineSeries { Title = "Price", YAxisKey = "PriceAxis", Color = OxyColors.SteelBlue };
+            MyPlotModel.Series.Add(priceSeries);
 
-                // SMA and bands
-                var smaSeries = new LineSeries { Title = s + " SMA14", YAxisKey = priceAxis.Key, Color = OxyColors.Orange };
-                var upperSeries = new LineSeries { Title = s + " Upper Band", YAxisKey = priceAxis.Key, Color = OxyColors.LightBlue };
-                var lowerSeries = new LineSeries { Title = s + " Lower Band", YAxisKey = priceAxis.Key, Color = OxyColors.LightBlue };
-                smaSeriesMap[s] = smaSeries;
-                upperBandSeriesMap[s] = upperSeries;
-                lowerBandSeriesMap[s] = lowerSeries;
-                MyPlotModel.Series.Add(smaSeries);
-                MyPlotModel.Series.Add(upperSeries);
-                MyPlotModel.Series.Add(lowerSeries);
+            // SMA series
+            smaSeries = new LineSeries { Title = "SMA", YAxisKey = "PriceAxis", Color = OxyColors.Red, StrokeThickness = 2 };
+            MyPlotModel.Series.Add(smaSeries);
 
-                // Volume
-                var vSeries = new LineSeries { Title = s + " Volume", YAxisKey = volAxis.Key };
-                volumeSeriesMap[s] = vSeries;
-                //MyPlotModel.Series.Add(vSeries);
+            // Bollinger bands
+            bollingerArea = new AreaSeries
+            {
+                Title = "Bollinger Bands",
+                YAxisKey = "PriceAxis",
+                Color = OxyColors.Transparent,
+                Fill = OxyColor.FromAColor(60, OxyColors.Green),
+                StrokeThickness = 1,
+                LineStyle = LineStyle.Dash
+            };
+            MyPlotModel.Series.Add(bollingerArea);
+
+            // Volume bars
+            volumeSeries = new RectangleBarSeries { Title = "Volume", YAxisKey = "VolumeAxis", FillColor = OxyColors.Gray };
+            MyPlotModel.Series.Add(volumeSeries);
+
+            // RSI line
+            rsiSeries = new LineSeries { Title = "RSI", YAxisKey = "RSIAxis", Color = OxyColors.Orange, StrokeThickness = 2 };
+            MyPlotModel.Series.Add(rsiSeries);
+
+            MyPlotView.Model = MyPlotModel;
+        }
+
+        private void GenerateInitialData()
+        {
+            pricePoints.Clear();
+            volumePoints.Clear();
+            double price = 100;
+
+            DateTime now = DateTime.Now;
+
+            for (int i = 0; i < maxPoints; i++)
+            {
+                price += rng.Next(-3, 4);
+                pricePoints.Add(new DataPoint(DateTimeAxis.ToDouble(now.AddSeconds(-maxPoints + i)), price));
+                volumePoints.Add(rng.Next(50, 150));
             }
 
-            // RSI Bands
-            MyPlotModel.Annotations.Add(new RectangleAnnotation { MinimumY = RSIOverbought, MaximumY = 100, Fill = OxyColor.FromAColor(40, OxyColors.Red) });
-            MyPlotModel.Annotations.Add(new RectangleAnnotation { MinimumY = 0, MaximumY = RSIOversold, Fill = OxyColor.FromAColor(40, OxyColors.Green) });
-
-            // Event wiring
-            engine.OnNewPrice += Engine_OnNewPrice;
-            engine.OnNewRSI += Engine_OnNewRSI;
-            engine.OnNewSMA += Engine_OnNewSMA;
-            engine.OnTrigger += Engine_OnTrigger;
-
-            engine.Start();
+            UpdateChart();
         }
 
-        private void Engine_OnNewPrice(string symbol, double price)
+        private void StartLiveDummyData()
         {
-            Dispatcher.Invoke(() =>
-            {
-                var series = priceSeriesMap[symbol];
-                series.Points.Add(new DataPoint(series.Points.Count, price));
-                MyPlotModel.InvalidatePlot(true);
-            });
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
-        private void Engine_OnNewRSI(string symbol, double rsi)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-            Dispatcher.Invoke(() =>
+            // New price
+            double lastPrice = pricePoints.Last().Y;
+            double newPrice = lastPrice + rng.Next(-3, 4);
+            DateTime now = DateTime.Now;
+
+            pricePoints.Add(new DataPoint(DateTimeAxis.ToDouble(now), newPrice));
+            volumePoints.Add(rng.Next(50, 150));
+
+            // Keep last maxPoints
+            if (pricePoints.Count > maxPoints)
             {
-                var series = rsiSeriesMap[symbol];
-                series.Points.Add(new DataPoint(series.Points.Count, rsi));
-                MyPlotModel.InvalidatePlot(true);
-            });
+                pricePoints.RemoveAt(0);
+                volumePoints.RemoveAt(0);
+            }
+
+            UpdateChart();
         }
 
-        private void Engine_OnNewSMA(string symbol, double sma, double upper, double lower)
+        private void UpdateChart()
         {
-            Dispatcher.Invoke(() =>
-            {
-                smaSeriesMap[symbol].Points.Add(new DataPoint(smaSeriesMap[symbol].Points.Count, sma));
-                upperBandSeriesMap[symbol].Points.Add(new DataPoint(upperBandSeriesMap[symbol].Points.Count, upper));
-                lowerBandSeriesMap[symbol].Points.Add(new DataPoint(lowerBandSeriesMap[symbol].Points.Count, lower));
-                MyPlotModel.InvalidatePlot(true);
-            });
-        }
+            priceSeries.Points.Clear();
+            smaSeries.Points.Clear();
+            bollingerArea.Points.Clear();
+            bollingerArea.Points2.Clear();
+            volumeSeries.Items.Clear();
+            rsiSeries.Points.Clear();
 
-        private void Engine_OnTrigger(TriggerHit hit)
-        {
-            Dispatcher.Invoke(() =>
+            for (int i = 0; i < pricePoints.Count; i++)
             {
-                var annotation = new PointAnnotation
+                var pt = pricePoints[i];
+                priceSeries.Points.Add(pt);
+
+                // Volume
+                volumeSeries.Items.Add(new RectangleBarItem(
+                    pt.X - 0.4 / 86400, 0, pt.X + 0.4 / 86400, volumePoints[i]
+                ));
+
+                if (i >= smaPeriod - 1)
                 {
-                    X = priceSeriesMap[hit.Symbol].Points.Count - 1,
-                    Y = hit.Price,
-                    Shape = hit.TriggerName == "TrendLong" ? MarkerType.Triangle : MarkerType.Square,
-                    Fill = hit.TriggerName == "TrendLong" ? OxyColors.Green : OxyColors.Red
-                };
-                MyPlotModel.Annotations.Add(annotation);
-                MyPlotModel.InvalidatePlot(true);
-            });
+                    var window = pricePoints.Skip(i - smaPeriod + 1).Take(smaPeriod).Select(p => p.Y).ToList();
+                    double sma = window.Average();
+                    smaSeries.Points.Add(new DataPoint(pt.X, sma));
+
+                    double stddev = Math.Sqrt(window.Sum(v => Math.Pow(v - sma, 2)) / (smaPeriod - 1));
+                    double upper = sma + bollingerMultiplier * stddev;
+                    double lower = sma - bollingerMultiplier * stddev;
+                    bollingerArea.Points.Add(new DataPoint(pt.X, upper));
+                    bollingerArea.Points2.Add(new DataPoint(pt.X, lower));
+
+                    // RSI
+                    double gains = 0, losses = 0;
+                    for (int j = 1; j < window.Count; j++)
+                    {
+                        double diff = window[j] - window[j - 1];
+                        if (diff > 0) gains += diff;
+                        else losses -= diff;
+                    }
+                    double rs = losses == 0 ? 100 : gains / losses;
+                    double rsi = 100 - (100 / (1 + rs));
+                    rsiSeries.Points.Add(new DataPoint(pt.X, rsi));
+                }
+            }
+
+            MyPlotModel.InvalidatePlot(true);
         }
     }
 }
